a7f529da59e49d45bdd8059619ae445285115192
vyvan0706 <150523177+vyvan0706@users.noreply.github.com>
2023-11-11 20:07:02 +0700

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -1,5 +1,7 @@
 import streamlit as st
-tab1, tab2, tab3 = st.tabs(["Math", "Video", "Lời kết"])
+from streamlit_chat import message
+import requests
+tab1, tab2, tab3,tab4 = st.tabs(["Math", "Video", "Lời kết",'test'])
 with tab1:
    st.header('Bé tập làm toán:smile::exclamation:')
    st.image('https://www.unicef.org/montenegro/sites/unicef.org.montenegro/files/styles/hero_tablet/public/his_sofija-05-19-235_0.jpg?itok=YJQLrCrq')
@@ -48,5 +50,48 @@ with tab3:
    st.write('Cuộc sống này không có điều gì đơn giản, quan trọng nhất là')  
    st.write(':rainbow[bản thân mình đã đạt được thành tựu to lớn nào hay ho]')
    st.write('hihi iu bn')
-   
-    
+with tab4:
+ st.set_page_config(
+    page_title="Streamlit Chat - Demo",
+    page_icon=":robot:"
+)
+
+  API_URL = "https://api-inference.huggingface.co/models/facebook/blenderbot-400M-distill"
+  headers = {"Authorization": st.secrets['api_key']}
+
+  st.header("Streamlit Chat - Demo")
+  st.markdown("[Github](https://github.com/ai-yash/st-chat)")
+
+  if 'generated' not in st.session_state:
+    st.session_state['generated'] = []
+
+  if 'past' not in st.session_state:
+    st.session_state['past'] = []
+
+  def query(payload):
+	response = requests.post(API_URL, headers=headers, json=payload)
+	return response.json()
+  def get_text():
+    input_text = st.text_input("You: ","Hello, how are you?", key="input")
+    return input_text 
+
+
+  user_input = get_text()
+
+  if user_input:
+    output = query({
+        "inputs": {
+            "past_user_inputs": st.session_state.past,
+            "generated_responses": st.session_state.generated,
+            "text": user_input,
+        },"parameters": {"repetition_penalty": 1.33},
+    })
+
+    st.session_state.past.append(user_input)
+    st.session_state.generated.append(output["generated_text"])
+
+  if st.session_state['generated']:
+
+    for i in range(len(st.session_state['generated'])-1, -1, -1):
+        message(st.session_state["generated"][i], key=str(i))
+        message(st.session_state['past'][i], is_user=True, key=str(i) + '_user')
