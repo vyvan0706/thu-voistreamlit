2c430627b72881547678bae33c7ab9acbbdece78
vyvan0706 <150523177+vyvan0706@users.noreply.github.com>
2023-11-11 19:25:50 +0700

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -1,51 +1,33 @@
-# Copyright (c) Streamlit Inc. (2018-2022) Snowflake Inc. (2022)
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 import streamlit as st
-from streamlit.logger import get_logger
-
-LOGGER = get_logger(__name__)
-
-
-def run():
-    st.set_page_config(
-        page_title="Hello",
-        page_icon="👋",
-    )
-
-    st.write("# Welcome to Streamlit! 👋")
-
-    st.sidebar.success("Select a demo above.")
-
-    st.markdown(
-        """
-        Streamlit is an open-source app framework built specifically for
-        Machine Learning and Data Science projects.
-        **👈 Select a demo from the sidebar** to see some examples
-        of what Streamlit can do!
-        ### Want to learn more?
-        - Check out [streamlit.io](https://streamlit.io)
-        - Jump into our [documentation](https://docs.streamlit.io)
-        - Ask a question in our [community
-          forums](https://discuss.streamlit.io)
-        ### See more complex demos
-        - Use a neural net to [analyze the Udacity Self-driving Car Image
-          Dataset](https://github.com/streamlit/demo-self-driving)
-        - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
-    """
-    )
-
-
-if __name__ == "__main__":
-    run()
+st.header(':rainbow [Bé tập làm toán]:smile:')
+col1,col2,col3=st.columns(3)
+with col1:
+   number = st.number_input('a',key='num1')
+with col2:
+   radio = st.radio('Phép toán', ('*+', '*-', 'x',':'),horizontal=True)
+with col3:
+  number2=st.number_input('b',key='num2')
+number3=st.number_input('Nhập kết quả',key='num3')
+if st.button('Kiểm tra'):
+   if radio=='x':
+      if number*number2==number3:
+        st.write('True')
+      else: 
+         st.write('False')
+   elif radio==':':
+      if number/number2==number3:
+        st.write('True')
+      else: 
+         st.write('False')
+   elif radio=='*-':
+      if number-number2==number3:
+        st.write('True')
+      else: 
+         st.write('False')
+   elif radio=='*+':
+      if number + number2==number3:
+        st.write('True')
+      else: 
+         st.write('False')
+         
+    
