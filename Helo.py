c19438fc4223b838ae55aa7ce786636943cf530a
vyvan0706 <150523177+vyvan0706@users.noreply.github.com>
2023-11-11 20:18:33 +0700

    Update Helo.py

--- Helo.py
+++ Helo.py
@@ -1,1 +1,38 @@
+import streamlit as st
+import gtts
+from ftlangdetect import detect as lang_detector
+from deep_translator import GoogleTranslator
 
+st.title(':blue[Lồng tiếng với chị Google]')
+user_input= st.text_area('Nhập văn bản muốn lồng tiếng')
+language_code_mapping={
+     'Tiếng Việt':'vi',
+     'Tiếng Anh':'en',
+     'Tiếng Hàn':'ko',
+     'Tiếng Nhật':'ja'
+} 
+translate = st.toggle('Dịch văn bản')
+if translate:
+     target_language= st.selectbox(
+    'Chọn ngôn ngữ của bản dịch',
+    ('Tiếng Việt','Tiếng Anh','Tiếng Hàn','Tiếng Nhật'))
+     target_language_code=language_code_mapping[target_language]
+submitbut=st.button('Xác nhận')
+if user_input and submitbut:
+     language_code=lang_detector(user_input)['lang']
+     result= gtts.gTTS(text=user_input, lang=language_code)
+     result.save('S2/tts_result/result.mp3')
+     col1,col2=st.columns(2)
+     with col1:
+       st.subheader('Văn bản gốc')
+       st.audio('S2/tts_result/result.mp3')
+     if translate:
+          translated_input = GoogleTranslator(source='auto', target=target_language_code).translate(user_input)  
+          trans_result= gtts.gTTS(text=translated_input, lang=target_language_code)
+          trans_result.save('S2/tts_result/trans_result.mp3')
+          with col2:
+            st.subheader('Văn bản dịch')
+            st.audio('S2/tts_result/trans_result.mp3')
+
+
+ 
