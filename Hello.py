5349953b4a4fe28cb09caa9f44c4e81e9e2d36d1
vyvan0706 <150523177+vyvan0706@users.noreply.github.com>
2023-11-11 19:53:49 +0700

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -1,44 +1,46 @@
 import streamlit as st
-st.title(('Đây là một video đặc sắc')
-st.video('https://youtu.be/qOih8EjXnx4?si=VW79yWbFEbiUl5Mo')
-st.header('Bé tập làm toán:smile::exclamation:')
-st.image('https://www.unicef.org/montenegro/sites/unicef.org.montenegro/files/styles/hero_tablet/public/his_sofija-05-19-235_0.jpg?itok=YJQLrCrq')
-col1,col2,col3=st.columns(3)
-with col1:
-   number = st.number_input('a',key='num1')
-with col2:
-   radio = st.radio('Phép toán', ('*+', '*-', 'x',':'),horizontal=True)
-with col3:
-  number2=st.number_input('b',key='num2')
-number3=st.number_input('Nhập kết quả',key='num3')
-if st.button('Kiểm tra'):
-   if radio=='x':
+tab1, tab2, tab3 = st.tabs(["Math", "Video", "Loi ket"])
+with tab1:
+   st.header('Bé tập làm toán:smile::exclamation:')
+   st.image('https://www.unicef.org/montenegro/sites/unicef.org.montenegro/files/styles/hero_tablet/public/his_sofija-05-19-235_0.jpg?itok=YJQLrCrq')
+   col1,col2,col3=st.columns(3)
+   with col1:
+      number = st.number_input('a',key='num1')
+   with col2:
+      radio = st.radio('Phép toán', ('*+', '*-', 'x',':'),horizontal=True)
+   with col3:
+      number2=st.number_input('b',key='num2')
+      number3=st.number_input('Nhập kết quả',key='num3')
+   if st.button('Kiểm tra'):
+     if radio=='x':
       if number*number2==number3:
         st.write('True')
         st.balloons()
         st.snow()
       else: 
          st.write('False')
-   elif radio==':':
-      if number/number2==number3:
+     elif radio==':':
+       if number/number2==number3:
         st.write('True')
         st.balloons()
         st.snow()
-      else: 
+       else: 
          st.write('False')
-   elif radio=='*-':
+     elif radio=='*-':
       if number-number2==number3:
         st.write('True')
         st.balloons()
         st.snow()
       else: 
          st.write('False')
-   elif radio=='*+':
+     elif radio=='*+':
       if number + number2==number3:
         st.write('True')
         st.balloons()
         st.snow()      
       else: 
          st.write('False')
-         
+with tab2:
+   st.title(('Đây là một video đặc sắc')
+   st.video('https://youtu.be/qOih8EjXnx4?si=VW79yWbFEbiUl5Mo')
     
