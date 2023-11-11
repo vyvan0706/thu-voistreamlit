1653b0b5479e84a70db75d70ae69df91075e0104
vyvan0706 <150523177+vyvan0706@users.noreply.github.com>
2023-11-11 20:30:49 +0700

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -1,5 +1,8 @@
 import streamlit as st
-tab1, tab2, tab3 = st.tabs(["Math", "Video", "Lời kết"])
+import gtts
+from ftlangdetect import detect as lang_detector
+from deep_translator import GoogleTranslator
+tab1, tab2, tab3 , tab4 = st.tabs(["Math", "Video", "Lời kết",'thu'])
 with tab1:
    st.header('Bé tập làm toán:smile::exclamation:')
    st.image('https://www.unicef.org/montenegro/sites/unicef.org.montenegro/files/styles/hero_tablet/public/his_sofija-05-19-235_0.jpg?itok=YJQLrCrq')
@@ -48,3 +51,31 @@ with tab3:
    st.write('Cuộc sống này không có điều gì đơn giản, quan trọng nhất là')  
    st.write(':rainbow[bản thân mình đã đạt được thành tựu to lớn nào hay ho]')
    st.write('hihi iu bn')
+with tab4:
+   st.title(':blue[Lồng tiếng với chị Google]')
+   user_input= st.text_area('Nhập văn bản muốn lồng tiếng')
+   language_code_mapping={
+     'Tiếng Việt':'vi',
+     'Tiếng Anh':'en',
+     'Tiếng Hàn':'ko',
+     'Tiếng Nhật':'ja'} 
+   translate = st.toggle('Dịch văn bản')
+   if translate:
+     target_language= st.selectbox( 'Chọn ngôn ngữ của bản dịch', ('Tiếng Việt','Tiếng Anh','Tiếng Hàn','Tiếng Nhật'))
+     target_language_code=language_code_mapping[target_language]
+     submitbut=st.button('Xác nhận')
+   if user_input and submitbut:
+     language_code=lang_detector(user_input)['lang']
+     result= gtts.gTTS(text=user_input, lang=language_code)
+     result.save('S2/tts_result/result.mp3')
+     col1,col2=st.columns(2)
+     with col1:
+       st.subheader('Văn bản gốc')
+       st.audio('S2/tts_result/result.mp3')
+     if translate:
+          translated_input = GoogleTranslator(source='auto', target=target_language_code).translate(user_input)  
+          trans_result= gtts.gTTS(text=translated_input, lang=target_language_code)
+          trans_result.save('S2/tts_result/trans_result.mp3')
+           with col2:
+            st.subheader('Văn bản dịch')
+            st.audio('S2/tts_result/trans_result.mp3')
